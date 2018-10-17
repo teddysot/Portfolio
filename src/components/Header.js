a1529ee0b181742c7540c02f927693f2f0bead6b
teddysot <earth_sot@hotmail.com>
2018-10-17 01:43:54 -0700

    change bg and main info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import pic from '../images/pic.jpg'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon fa-address-book"></span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Saharat Nasahachart</h1>
+                <h1>(Teddy)</h1>
+                <p>Software Developer</p>
             </div>
         </div>
         <nav>
